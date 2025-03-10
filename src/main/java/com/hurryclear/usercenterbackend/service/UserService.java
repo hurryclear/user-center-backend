f5888c762d39a57d5b6bb5836fdc920a045eb74e
hurryclear <hurryclear@mail.de>
2025-03-10 20:09:38 +0100

    implement registration function

--- src/main/java/com/hurryclear/usercenterbackend/service/UserService.java
+++ src/main/java/com/hurryclear/usercenterbackend/service/UserService.java
@@ -10,4 +10,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface UserService extends IService<User> {
 
+    long userRegister(String userAccount, String userPassword, String checkPassword);
 }
--- src/main/java/com/hurryclear/usercenterbackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/hurryclear/usercenterbackend/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hurryclear.usercenterbackend.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hurryclear.usercenterbackend.model.domain.User;
 import com.hurryclear.usercenterbackend.service.UserService;
 import com.hurryclear.usercenterbackend.mapper.UserMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
 * @author hurjiang
@@ -15,6 +22,64 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1. check not null --> using StringUtils.isAnyBlank from Apache Commons Lang
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return -1;
+        }
+
+        // 2. check userAccount not shorter than 4 digits
+        if (userAccount.length() < 4 ) {
+            return -1;
+        }
+        // 3. check userPassword not shorter than 8 digits
+        if (userPassword.length() < 8) {
+            return -1;
+        }
+
+        // 5. no special character in userAccount
+        String invalidPattern = "\\pP|\\pS|\\s+";
+        Matcher matcher = Pattern.compile(invalidPattern).matcher(userAccount);
+        if (matcher.find()) {
+            return -1;
+        }
+
+        // 6. userPassword same with checkPassword
+        if (! userPassword.equals(checkPassword)) {
+            return -1;
+        }
+
+        // 4. unique userAccount --> method 1: QueryWrapper in MyBatis-plus
+        // Why put this step at last? because we do query once here in database, if something wrong before this, then no need to query, save resource
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        long count = userMapper.selectCount(queryWrapper);
+        if(count > 0) {
+            return -1;
+        }
+//        if (userRepository.existsByUserAccount(userAccount)) {
+//            return -1;
+//        }
+
+        // 7. encrypt password --> md5
+        final String SALT = "hurryclear";
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+
+        // 8. save new user in database
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        boolean saveResult = this.save(user);
+        if (! saveResult) {
+            return -1;
+        }
+
+        return user.getId();
+    }
 }
 
 

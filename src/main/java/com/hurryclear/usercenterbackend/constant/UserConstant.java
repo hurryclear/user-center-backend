e46645f17a1e726a53971b6311b051b47ab2aefe
hurryclear <hurryclear@mail.de>
2025-03-11 17:44:08 +0100

    add USER_LOGIN_STATE and ADMIN_ROLE, DEFAULT_ROLE

---
+++ src/main/java/com/hurryclear/usercenterbackend/constant/UserConstant.java
@@ -0,0 +1,10 @@
+package com.hurryclear.usercenterbackend.constant;
+
+public interface UserConstant {
+
+    String USER_LOGIN_STATE = "userLoginState";
+
+    // role
+    int DEFAULT_ROLE = 0;
+    int ADMIN_ROLE = 1;
+}
